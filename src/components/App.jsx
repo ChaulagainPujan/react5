ce288eb54b3d02b7fd7b1dbbaf1b637e37ad145b
ChaulagainPujan <46419945+ChaulagainPujan@users.noreply.github.com>
2020-12-04 13:08:17 +0545

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header.jsx";
+import List from "./List.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default App;
